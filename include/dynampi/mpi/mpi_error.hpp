b86bb745b3160d2a064a56a021220dd20500d391
Ryan Stocks <ryan.stocks00@gmail.com>
2025-08-18 17:54:56 +1000

    Allow both source location and experimental source location to be missing

--- include/dynampi/mpi/mpi_error.hpp
+++ include/dynampi/mpi/mpi_error.hpp
@@ -9,8 +9,10 @@
 
 #if __has_include(<source_location>)
 #include <source_location>
-#else
+#elif __has_include(<experimental/source_location>)
 #include <experimental/source_location>
+#else
+#define NO_SOURCE_LOCATION
 #endif
 #include <stdexcept>
 
@@ -24,14 +26,20 @@
 
 namespace dynampi {
 inline void mpi_check(int err, std::string_view command,
-                      std::source_location loc = std::source_location::current()) {
+#ifndef NO_SOURCE_LOCATION
+                      std::source_location loc = std::source_location::current()
+#endif
+) {
   if (err != MPI_SUCCESS) {
     char error_string[MPI_MAX_ERROR_STRING];
     int length_of_error_string;
     MPI_Error_string(err, error_string, &length_of_error_string);
     throw std::runtime_error(std::string("MPI error in ") + std::string(command) + ": " +
                              std::string(error_string, length_of_error_string) + " at " +
-                             loc.file_name() + ":" + std::to_string(loc.line()));
+#ifndef NO_SOURCE_LOCATION
+                             loc.file_name() + ":" + std::to_string(loc.line())
+#endif
+    );
   }
 }
 }  // namespace dynampi
