d0a8e93a1387551afcd36d11af249687cc340078
Ryan Stocks <ryan.stocks00@gmail.com>
2026-02-05 00:18:40 +1100

    Restoring some test elements that were nuked

--- test/mpi/test_distributers.cpp
+++ test/mpi/test_distributers.cpp
@@ -30,7 +30,7 @@ template <typename T>
 class DynamicDistribution : public ::testing::Test {
  protected:
   template <typename TaskT, typename ResultT, typename... Options>
-  auto make_distributor(auto worker_task) {
+  auto make_distributor(auto worker_task, bool auto_run = false) {
     using DistT = typename T::template type<TaskT, ResultT, Options...>;
 
     // Use decltype to get the correct Options type regardless of its internal name
@@ -38,7 +38,7 @@ class DynamicDistribution : public ::testing::Test {
 
     ConfigT opts{};
     opts.comm = MPI_COMM_WORLD;
-    opts.auto_run_workers = false;
+    opts.auto_run_workers = auto_run;
 
     if constexpr (T::use_immediate_recv) {
       opts.use_immediate_recv = true;
@@ -120,9 +120,41 @@ TYPED_TEST(DynamicDistribution, Statistics) {
     auto results = dist.finish_remaining_tasks();
 
     size_t expected_size = (MPIEnvironment::world_comm_size() == 1) ? 0 : 5;
+    EXPECT_EQ(results, (std::vector<int>{1, 4, 9, 16, 25}));
     EXPECT_EQ(dist.get_statistics().comm_statistics.send_count, expected_size);
+    EXPECT_EQ(dist.get_statistics().comm_statistics.bytes_sent, expected_size * sizeof(int));
+    EXPECT_EQ(dist.get_statistics().comm_statistics.recv_count,
+              expected_size + MPIEnvironment::world_comm_size() - 1);
+    EXPECT_EQ(dist.get_statistics().comm_statistics.bytes_received, expected_size * sizeof(int));
+
     dist.finalize();
+    EXPECT_EQ(dist.get_statistics().comm_statistics.send_count,
+              expected_size + MPIEnvironment::world_comm_size() - 1);
+    EXPECT_EQ(dist.get_statistics().comm_statistics.bytes_sent, expected_size * sizeof(int));
+
+    double expected_num_bytes = 0;
+    if (MPIEnvironment::world_comm_size() > 1) {
+      expected_num_bytes = static_cast<double>(expected_size * sizeof(int)) /
+                           (expected_size + MPIEnvironment::world_comm_size() - 1);
+    }
+    EXPECT_DOUBLE_EQ(dist.get_statistics().comm_statistics.average_receive_size(),
+                     expected_num_bytes);
+    EXPECT_DOUBLE_EQ(dist.get_statistics().comm_statistics.average_send_size(), expected_num_bytes);
   } else {
     dist.run_worker();
   }
+}
+
+TYPED_TEST(DynamicDistribution, AutoRunWorkers) {
+  auto worker_task = [](int task) -> int { return task * task; };
+  // Test with auto_run_workers = true - workers should start automatically
+  auto dist = this->template make_distributor<int, int>(worker_task, true);
+
+  if (dist.is_root_manager()) {
+    // Workers should already be running, so we can just insert tasks
+    dist.insert_tasks({1, 2, 3, 4, 5});
+    auto results = dist.finish_remaining_tasks();
+    EXPECT_EQ(results, (std::vector<int>{1, 4, 9, 16, 25}));
+  }
+  // Workers run automatically in constructor, no need to call run_worker()
 }
